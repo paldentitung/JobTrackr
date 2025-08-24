0b224145d8718cd7c423cfbf591d627f707bbb13
Palden D. Titung <paldendorjetitung@gmail.com>
2025-08-24 17:20:20 +0545

    structure  done

--- index.js
+++ index.js
@@ -8,3 +8,85 @@ menuBtn.addEventListener("click", () => {
     ? '<i class="fas fa-times" ></i>'
     : '  <i class="fas fa-bars" id="menu-icon"></i>';
 });
+
+async function getJobDetails() {
+  try {
+    let res = await fetch("http://localhost:3000/jobs");
+    let data = await res.json();
+    renderJobDetails(data);
+    showMoreButtonFunction(data);
+  } catch (error) {
+    alert("error is " + error);
+  }
+}
+
+function renderJobDetails(data) {
+  let cartContainerHtml = "";
+  data.forEach((job) => {
+    cartContainerHtml += `
+          <div class="card">
+            <div class="job-type">${job.type}</div>
+            <div class="job-title">${job.title}</div>
+            <div class="job-description">
+          ${job.description}
+            </div>
+            <div class="job-details">
+              <div class="job-salary">$${job.salary}/year</div>
+              <div class="job-location-box">
+                loaction: <span class="job-loaction">${job.location}</span>
+              </div>
+            </div>
+            <button class="read-more-btn">Read More</button>
+          </div>
+  `;
+  });
+  document.querySelector(".job-card-container").innerHTML = cartContainerHtml;
+  let description = document.querySelectorAll(".job-description");
+
+  description.forEach((description) => {
+    let fullText = description.textContent;
+    let shortText = fullText.slice(0, 110) + "  ... ";
+
+    description.textContent = shortText;
+
+    description.addEventListener("click", () => {
+      if (description.textContent === fullText) {
+        description.textContent = shortText;
+      } else {
+        description.textContent = fullText;
+      }
+    });
+  });
+}
+
+function showMoreButtonFunction(data) {
+  let showMoreButton = document.getElementById("showmore-button");
+  let count = 3;
+  let showAll = false;
+
+  renderJobDetails(data.slice(0, count));
+  showMoreButton.addEventListener("click", () => {
+    if (!showAll) {
+      count = data.length;
+      renderJobDetails(data.slice(onabort, count));
+      showMoreButton.textContent = "show less";
+      showAll = true;
+    } else {
+      count = 3;
+      renderJobDetails(data.slice(0, count));
+      showAll = false;
+    }
+  });
+}
+getJobDetails();
+
+// faq
+
+const faqbox = document.querySelectorAll(".faq-item");
+
+faqbox.forEach((question) => {
+  question.addEventListener("click", () => {
+    let ans = question.querySelector(".faq-answer");
+    ans.classList.toggle("active");
+  });
+});
